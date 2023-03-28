07235fe74b7493b9c5829056e2c473370be6eedc
Dmitri Mandaji <badmopss@gmail.com>
2023-03-28 16:30:10 +0200

    Add class construction to the class a Person

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -6,9 +6,40 @@ package Person;
 // Но можно сравнивать даты рождения!
 // Альтернативная подсказка: если очень хочется вычислять возраст,
 // то можно это делать в миллисекундах с использованием getTime()!
-public class Person {
+public class Person implements Comparable<Person> {
 
   private String name;
   private int birthDate;
   private int age;
+
+  public Person(String name, int birthDate, int age) {
+    this.name = name;
+    this.birthDate = birthDate;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setBirthDate(int birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public int getBirthDate() {
+    return birthDate;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
 }
+
